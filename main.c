ceef4bfc6aef171422c122f26d763e223074133f
Илья Старцев <118308092+glfksd@users.noreply.github.com>
2023-02-15 14:49:22 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+/*Задание 2. Array18.*/
+
+#include <stdio.h>
+int main()
+{
+   int a[10];
+   int b;
+    for (b=0; b<10; ++b){
+       printf("a[%d] : ",b+1);
+       scanf("%d",&a[b]);
+    }
+    
+    k = 9;
+    while (((a[0]>=a[b])||(a[b]>=a[9])) && (k>0))
+    {
+        --b;
+    }
+    
+    if (b==0)
+    
+    printf("%d", b);
+    
+      else  
+      
+      printf("%d",a[b]);
+      
+    return 0;
+} 
+    
+    
+    
+    
+
